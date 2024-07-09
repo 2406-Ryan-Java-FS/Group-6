ac8ab53ea7910c897b7b39bee66b75a457e37d22
jaydel14 <70174605+jaydel14@users.noreply.github.com>
2024-07-09 09:04:04 -0600

    added spring boot

---
+++ Project_1/src/main/java/com/group6/revature/app/Project1Application.java
@@ -0,0 +1,13 @@
+package com.group6.revature.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project1Application.class, args);
+	}
+
+}
---
+++ Project_1/src/test/java/com/group6/revature/app/Project1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group6.revature.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
