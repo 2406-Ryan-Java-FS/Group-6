fcb307e5ed12745bbdb7f9faa7817db143a0da52
Victor Galbraith <victorg87@gmail.com>
2024-07-10 13:24:50 -0700

    Updated existing repositories to extend JpaRepository

--- Project_1/src/main/java/com/group6/revature/repository/vehicleMakesModelsRepo.java
+++ Project_1/src/main/java/com/group6/revature/repository/vehicleMakesModelsRepo.java
@@ -1,8 +1,9 @@
 package com.group6.revature.repository;
 
-//import org.springframework.data.jpa.repository.JpaRepository;
+import com.group6.revature.model.VehicleMakesModels;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface vehicleMakesModelsRepo {
+public interface vehicleMakesModelsRepo extends JpaRepository<VehicleMakesModels, Integer> {
 }
