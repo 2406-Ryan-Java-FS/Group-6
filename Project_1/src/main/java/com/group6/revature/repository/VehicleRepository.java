560475cce0e3f0131442874399b27e6ea1ee1317
Victor Galbraith <VictorG87@Gmail.com>
2024-07-11 16:18:51 -0700

    Update VehicleRepository.java

--- Project_1/src/main/java/com/group6/revature/repository/VehicleRepository.java
+++ Project_1/src/main/java/com/group6/revature/repository/VehicleRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+public interface VehicleRepository extends JpaRepository<Vehicle, Integer> {
 }
