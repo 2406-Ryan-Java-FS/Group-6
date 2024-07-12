3ae58c33a91e1d2320eecf1bb5db9354ae7546a3
Victor Galbraith <victorg87@gmail.com>
2024-07-12 08:24:20 -0700

    Added check to addVehicle() for valid customer_id

--- Project_1/src/main/java/com/group6/revature/service/VehicleService.java
+++ Project_1/src/main/java/com/group6/revature/service/VehicleService.java
@@ -1,21 +1,38 @@
 package com.group6.revature.service;
 
+import com.group6.revature.exception.BadRequestException;
 import com.group6.revature.model.Vehicle;
 import com.group6.revature.repository.VehicleRepository;
+import com.group6.revature.repository.userRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class VehicleService {
 
     private final VehicleRepository vehicleRepository;
+    private final userRepo userRepository;
 
     @Autowired
-    public VehicleService(VehicleRepository vehicleRepository) {
+    public VehicleService(VehicleRepository vehicleRepository, userRepo userRepository) {
         this.vehicleRepository = vehicleRepository;
+        this.userRepository = userRepository;
     }
 
+    /**
+     * Used to persist a Vehicle to the repository.
+     *
+     * @param vehicle The Vehicle to be added.
+     * @return The persisted Vehicle including it's newly assigned game_id.
+     * @throws BadRequestException if there's an issue with the client's request.
+     */
     public Vehicle addVehicle(Vehicle vehicle) {
+
+        if (vehicle.getCustomerId() != null && !userRepository.existsById(vehicle.getCustomerId())) {
+            throw new BadRequestException("Customer does not exist.");
+        }
         return vehicleRepository.save(vehicle);
     }
 }
