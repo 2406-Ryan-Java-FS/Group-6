3e8bbeb94dcd42c7971a0e62616168c0a074ab50
jaydel14 <70174605+jaydel14@users.noreply.github.com>
2024-07-17 09:26:34 -0400

    Nav bar

---
+++ frontend/src/components/home-Manager.js
@@ -0,0 +1,6 @@
+export default function Home(){
+
+    return(<>
+        <h1>Auto Parts Store</h1>
+    </>)
+}
---
+++ frontend/src/components/nav-bar.js
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+export default function NavBar(){
+
+    return(<>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/">AutoParts</Link>
+                <Link className="nav-link" to="/parts">Parts</Link>
+                <Link className="nav-link" to="/">Place Order</Link>
+                <Link className="nav-link" to="/">View Order</Link>
+                <Link className="nav-link" to="/">Login</Link>
+                {/* <Link className="nav-link" to="#">Pricing</Link> */}
+                <form className="d-flex" role="search">
+                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                    <button className="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </nav>
+    </>)
+}
