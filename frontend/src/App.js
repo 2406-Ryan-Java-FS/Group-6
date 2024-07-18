09f36eaa6f38460b1fc68500611ec613fb46969c
jaydel14 <70174605+jaydel14@users.noreply.github.com>
2024-07-18 17:59:21 -0400

    made place orders page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import NavBar from './components/nav-bar';
 import Home from './components/home-Manager';
 import PartsManager from './components/parts-manager';
+import PlaceOrder from './components/plcOrder-Manager';
 import UserSettings from './components/user-settings';
 
 
@@ -15,8 +16,8 @@ function App() {
     <Routes>
       <Route path='' element={<Home/>} />
       <Route path='/parts' element={<PartsManager/>} />
+      <Route path='/orders' element={<PlaceOrder/>} />
       <Route path='/settings' element={<UserSettings/>} />
-
     </Routes>
   </>)
   
---
+++ frontend/src/components/plcOrder-Manager.js
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import '../styles/plcOrder.css'
+import '../styles/nav.css'
+
+export default function PlaceOrder(){
+
+
+
+
+    return(<>
+        <h1>AutoParts Place Order</h1>
+    </>);
+}
