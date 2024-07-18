6074c715bec852fffdffd8e832582339da12d80c
Victor Galbraith <victorg87@gmail.com>
2024-07-18 14:57:36 -0700

    initial login page

---
+++ frontend/src/components/login-page.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import '../styles/login.css';
+
+export default function LoginPage() {
+
+    return (
+        <>
+            <div className="headLogin">
+                <h1>Login</h1>
+            </div>
+        </>
+    );
+}
