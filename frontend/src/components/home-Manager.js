d12a31e2ff1106510ad72c69186eaa456e0dcee8
jaydel14 <70174605+jaydel14@users.noreply.github.com>
2024-07-17 23:45:28 -0400

    finished style for home

--- frontend/src/components/home-Manager.js
+++ frontend/src/components/home-Manager.js
@@ -8,7 +8,8 @@ export default function Home(){
             <h1>Auto Parts Store</h1>    
         </div>
         <div className="introHome">
-            <p>Welcome to AutoParts Store, your ultimate destination for high-quality auto parts 
+            <p>
+                Welcome to AutoParts Store, your ultimate destination for high-quality auto parts 
                 and accessories. Whether you're a professional mechanic or a DIY enthusiast, we offer 
                 an extensive selection of parts for all makes and models. Our commitment to exceptional 
                 service and unbeatable prices ensures you find exactly what you need to keep your 
@@ -32,20 +33,41 @@ export default function Home(){
             <div className="partsHome-content">
                 <div className="partsCatagories">
                     <div className="group1">
-                        <div className="partsOil">
+                        {/* <div className="partsOil">
                             
                         </div>  
                         <div className="partsMechanical">
                             
-                        </div> 
+                        </div>  */}
+                         {/* <button className="partsButton partsOil" ></button><br></br>
+                         <div className="partsText">Oil</div>
+                         <button className="partsButton partsMechanical">Mechanical</button> */}
+                        <div className="partsContainer">
+                            <button className="partsButton partsOil"></button>
+                            <div className="partsText">Oil Parts</div>
+                        </div>
+                        <div className="partsContainer">
+                            <button className="partsButton partsMechanical"></button>
+                            <div className="partsText">Mechanical Parts</div>
+                        </div>
                     </div>
                     <div className="group2">
-                        <div className="partsLights">
+                        {/* <div className="partsLights">
                             
                         </div> 
                         <div className="partsRandom">
                             
-                        </div>  
+                        </div>   */}
+                        {/* <button className="partsButton partsLights"><span>Lights</span></button>
+                        <button className="partsButton partsRandom">Random</button> */}
+                        <div className="partsContainer">
+                            <button className="partsButton partsLights"></button>
+                            <div className="partsText">Light Bulbs</div>
+                        </div>
+                        <div className="partsContainer">
+                            <button className="partsButton partsRandom"></button>
+                            <div className="partsText">Other Parts</div>
+                        </div>
                     </div>                                  
                 </div>
             </div>
