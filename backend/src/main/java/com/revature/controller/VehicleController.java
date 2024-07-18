5f120f4bf8d81ed14c18c28228a9187a05051feb
Victor Galbraith <victorg87@gmail.com>
2024-07-17 17:21:59 -0700

    improved constraint checking and error handling for Vehicle Controller and Service

--- backend/src/main/java/com/revature/controller/VehicleController.java
+++ backend/src/main/java/com/revature/controller/VehicleController.java
@@ -1,6 +1,7 @@
 package com.revature.controller;
 
 import com.revature.exception.BadRequestException;
+import com.revature.exception.NotFoundException;
 import com.revature.model.Vehicle;
 import com.revature.service.VehicleService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,46 +25,53 @@ public class VehicleController {
     /**
      * Endpoint for registering a new Vehicle.
      *
-     * @param vehicleToRegister The Vehicle to be registered.
-     * @return The persisted Vehicle Object including it's newly assigned makeModel_id.
+     * @param vehicle The Vehicle to be registered.
+     * @return If successful, returns the persisted Vehicle with its newly assigned makeModelId, along with a 201 status code.
+     * If unsuccessful, returns a String message indicating the failure reason, along with a 400 or 404 status code.
      */
     @PostMapping
-    public ResponseEntity<Object> addVehicle(@RequestBody Vehicle vehicleToRegister) {
+    public ResponseEntity<Object> addVehicle(@RequestBody Vehicle vehicle) {
         try {
-            Vehicle registeredVehicle = vehicleService.addVehicle(vehicleToRegister);
+            Vehicle registeredVehicle = vehicleService.addVehicle(vehicle);
             return new ResponseEntity<>(registeredVehicle, HttpStatus.CREATED);
+        } catch (BadRequestException bre) {
+            return new ResponseEntity<>(bre.getMessage(), HttpStatus.BAD_REQUEST);
+        } catch (NotFoundException nfe) {
+            return new ResponseEntity<>(nfe.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
+
+    /**
+     * Endpoint for updating a Vehicle given its makeModelId.
+     *
+     * @param makeModelId The makeModelId of the registered Vehicle to be updated.
+     * @param vehicle     The Vehicle object containing the updated data.
+     * @return If successful, returns the updated Vehicle, along with a 200 status code.
+     * If unsuccessful, returns a String message indicating the failure reason, along with a 400 status code.
+     */
+    @PatchMapping("/{makeModelId}")
+    public ResponseEntity<Object> updateVehicle(@PathVariable Integer makeModelId, @RequestBody Vehicle vehicle) {
+        try {
+            Vehicle updatedVehicle = vehicleService.updateVehicle(makeModelId, vehicle);
+            return new ResponseEntity<>(updatedVehicle, HttpStatus.OK);
         } catch (BadRequestException bre) {
             return new ResponseEntity<>(bre.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
 
     /**
-     * Endpoint for updating a Vehicle given it's makeModelId.
+     * Endpoint for deleting a Vehicle given its makeModelId.
      *
-     * @param makeModelId The makeModelId of a registered Vehicle.
-     * @param vehicle     containing Vehicle data to be updated.
-     * @return The updated Vehicle.
-     */
-    @PatchMapping("/{makeModelId}")
-    public ResponseEntity<Vehicle> updateVehicle(@PathVariable Integer makeModelId, @RequestBody Vehicle vehicle) {
-        Vehicle updatedVehicle = vehicleService.updateVehicle(makeModelId, vehicle);
-        return new ResponseEntity<>(updatedVehicle, HttpStatus.OK);
-    }
-
-    /**
-     * Endpoint for deleting a Vehicle given it's makeModelId.
-     *
-     * @param makeModelId The makeModelId of Vehicle to be deleted.
-     * @return The number of rows affected.
+     * @param makeModelId The makeModelId of the Vehicle to be deleted.
+     * @return Returns a 200 status code if the deletion is successful, and a 404 status code if the Vehicle is not found.
      */
     @DeleteMapping("/{makeModelId}")
-    public ResponseEntity<Integer> deleteVehicle(@PathVariable Integer makeModelId) {
-        int rows = vehicleService.deleteVehicle(makeModelId);
+    public ResponseEntity<HttpStatus> deleteVehicle(@PathVariable Integer makeModelId) {
 
-        if (rows != 0) {
-            return new ResponseEntity<>(rows, HttpStatus.OK);
+        if (vehicleService.deleteVehicle(makeModelId)) {
+            return new ResponseEntity<>(HttpStatus.OK);
         } else {
-            return new ResponseEntity<>(rows, HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
@@ -79,14 +87,19 @@ public class VehicleController {
     }
 
     /**
-     * Endpoint for retrieving all Vehicles registered to a particular User.
+     * Endpoint for retrieving all Vehicles registered to a specified User.
      *
      * @param customerId The userId of a registered User.
-     * @return A list of all Vehicles registered to the applicable User.
+     * @return If successful, returns a list of all Vehicles registered to the specified User.
+     * If unsuccessful, returns a String message indicating the failure reason, along with a 400 status code.
      */
     @GetMapping("/{customerId}")
-    public ResponseEntity<List<Vehicle>> getVehiclesByCustomerId(@PathVariable Integer customerId) {
-        List<Vehicle> vehicles = vehicleService.getVehiclesByCustomerId(customerId);
-        return new ResponseEntity<>(vehicles, HttpStatus.OK);
+    public ResponseEntity<Object> getVehiclesByCustomerId(@PathVariable Integer customerId) {
+        try {
+            List<Vehicle> vehicles = vehicleService.getVehiclesByCustomerId(customerId);
+            return new ResponseEntity<>(vehicles, HttpStatus.OK);
+        } catch (BadRequestException bre) {
+            return new ResponseEntity<>(bre.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 }
