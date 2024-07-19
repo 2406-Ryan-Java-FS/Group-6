8daefbe267df8fbf605a0ca345cc03ecfdd59565
Victor Galbraith <victorg87@gmail.com>
2024-07-19 07:49:29 -0700

    working login page that stores token

--- frontend/src/components/login-page.js
+++ frontend/src/components/login-page.js
@@ -5,13 +5,54 @@ export default function LoginPage() {
 
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
 
-    const handleSubmit = () => {
+    async function login() {
+        console.log("Logging in...")
 
-        // TODO: send request to backend to login
+        const url = "http://localhost:8080/auth/login";
+        const credentials = { username, password };
 
+        try {
+            console.log(credentials);
+
+            const httpResponse = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(credentials),
+            });
+
+
+            if (!httpResponse.ok) {
+                throw new Error('Login failed');
+            }
+
+            const data = await httpResponse.json();
+            const accessToken = data.accessToken;
+
+            // Store the access token in localStorage
+            localStorage.setItem('accessToken', accessToken);
+
+            console.log('Access Token:', accessToken);
+            // TODO: Redirect user to the appropriate page after successful login
+
+        } catch (error) {
+            setError(error.message);
+            console.error('Error:', error);
+        }
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
         console.log('Username:', username);
         console.log('Password:', password);
+
+        login();
+
+        // can now access the accessToken via localStorage:
+        console.log(localStorage.accessToken);
     };
 
     return (
