0f828ab6445efeed99fbe457bab775b4653c2418
Nick4Reva <nickfeldman4info@gmail.com>
2024-07-21 17:38:17 -0500

    Looks alright, runs except for updating which I wll have to come back to because I can't pass id to that page.

--- frontend/src/components/order-manager.js
+++ frontend/src/components/order-manager.js
@@ -1,144 +1,11 @@
-// import { useEffect, useContext, useState, useRef } from 'react';
-// //import { AppContext } from '../AppContext';
 
-
-
-
-
-// export default function OrderTable() {
-
-//     const orderInput = useRef();
-
-//     const [orders, setOrders] = useState([]);
-//     const orderTableRows = orders.map(o =>
-//         <tr key={o.orderId}>
-//             <td>{o.orderId}</td>
-//             <td>{o.customerId}</td>
-//             <td>{o.orderDate}</td>
-//             <td>{o.status}</td>
-//             <td>{o.total}</td>
-//             <td>{o.partId}</td>
-//             <td>{o.quantity}</td>
-//             <td><button onClick = {() => updateOrder(o)} >Update Order</button></td>
-//             <td><button onClick = {() => deleteOrder(o)} >Cancel Order</button></td>
-//         </tr>
-//     );
-
-
-//     async function getAllOrders() {
-//         console.log("Getting Orders....")
-//         try {
-//             const url = "http://localhost:8080/orders";
-//             const httpResponse = await fetch(url, { 
-//                 method: 'GET',  
-//                 mode: 'cors'});
-//             console.log(httpResponse.status);
-//             const orderList = await httpResponse.json();
-
-//             setOrders(orderList);
-//         } catch (e) {
-//             console.log(e);
-//         }
-//     }
-    
-//     async function viewOrder() {
-//         console.log(orderInput.current.value);
-
-//         try {
-//             const url = `http://localhost:8080/orders/${orderInput.current.value}`;
-//             const httpResponse = await fetch(url, { 
-//                 method: 'GET',  
-//                 mode: 'cors'});
-//             console.log(httpResponse.status);
-//             const body = await httpResponse.json();
-//             let orderList = [];
-//             console.log(orderList);
-//             console.log(typeof(orderList));
-//             orderList.push(body);
-//             setOrders(orderList);
-//         } catch (e) {
-//             console.log(e);
-//         }
-//         orderInput.current.value = "";
-//     }
-
-//     function updateOrder(or) {
-//         console.log(or)
-//         return(<>
-//             <td>{or.orderId}</td>
-//             <td>{or.customerId}</td>
-//             <td>{or.orderDate}</td>
-//             <td>{or.status}</td>
-//             <td>{or.total}</td>
-//             <td>{or.partId}</td>
-//             <td>{or.quantity}</td>
-//         </>)
-//     }
-
-//     async function deleteOrder(o) {
-//         console.log(o);
-//         try {
-//             const url = `http://localhost:8080/orders/${o.orderId}`;
-//             const httpResponse = await fetch(url, { 
-//                 method: 'DELETE',  
-//                 mode: 'cors'});
-//             console.log(httpResponse.status);
-//             const body = await httpResponse.json();
-            
-//         } catch (e) {
-//             console.log(e);}
-//         try {
-//                 const url = "http://localhost:8080/orders";
-//                 const httpResponse = await fetch(url, { 
-//                     method: 'GET',  
-//                     mode: 'cors'});
-//                 console.log(httpResponse.status);
-//                 const orderList = await httpResponse.json();
-    
-//                 setOrders(orderList);
-//             } catch (e) {
-//                 console.log(e);
-//             }
-//     }
-    
-
-//     const tableStyle = {
-//         backgroundColor: "lightgrey",
-//         border: "1px solid black"
-//     }
-
-//     return (<>
-        
-        
-//         <button onClick={getAllOrders}>Get All Orders</button>     
-//         <input type="number" ref={orderInput} />
-//         <button onClick={viewOrder}>Enter Number of Order</button>
-
-//         {orders.length > 0 && (
-//             <table style = {tableStyle}>
-//                 <thead>
-//                     <tr>
-//                         <th>ID</th>
-//                         <th>Customer ID</th>
-//                         <th>Order Date</th>
-//                         <th>Status</th>
-//                         <th>Total</th>
-//                         <th>Part ID</th>
-//                         <th>Quantity</th>
-//                     </tr>
-//                 </thead>
-//                 <tbody>
-//                     {orderTableRows}
-//                 </tbody>
-//             </table>
-//         )}
-//     </>);
-
-// }
 
 import { useEffect, useContext, useState, useRef } from 'react';
 //import { AppContext } from '../AppContext';
 import PlaceOrder from './plcOrder-Manager';
+import Table from 'react-bootstrap/Table';
+import '../styles/parts.css'
+import '../styles/nav.css'
 
 
 export default function OrderTable() {
@@ -156,9 +23,9 @@ export default function OrderTable() {
       });
       return df;
     }
-    const [orders, setOrders] = useState([]);
+    const [orders, setOrders] = useState([]); 
     const orderTableRows = orders.map(o =>
-        <tr key={o.orderId}>
+        <tr key={parseInt(o.orderId)}>
             <td>{o.orderId}</td>
             <td>{o.customerId}</td>
             <td>{formDate(o.orderDate)}</td>
@@ -166,10 +33,11 @@ export default function OrderTable() {
             <td>{o.total}</td>
             <td>{o.partId}</td>
             <td>{o.quantity}</td>
-            <td><button onClick = {() => updateOrder(o)} >Update Order</button></td>
-            <td><button onClick = {() => deleteOrder(o)} >Cancel Order</button></td>
-        </tr>
-    );
+            <td><button onClick = {() => updateOrder(o)} class = "buttons" >Update Order</button></td>
+            <td><button onClick = {() => deleteOrder(o)} class = "buttons">Cancel Order</button></td>
+        </tr>);
+        
+       
 
 
     async function getAllOrders() {
@@ -200,29 +68,26 @@ export default function OrderTable() {
                 method: 'GET',  
                 mode: 'cors'});
             console.log(httpResponse.status);
-            const body = await httpResponse.json();
-            let orderList = [];
-            console.log(orderList);
-            console.log(typeof(orderList));
-            orderList.push(body);
-            setOrders(orderList);
+            if (httpResponse.status !=200) {
+                alert("No Order Found");
+            } else {
+                const body = await httpResponse.json();
+                let orderList = [];
+                console.log(orderList);
+                console.log(typeof(orderList));
+                orderList.push(body);
+                setOrders(orderList);
+            }
         } catch (e) {
             console.log(e);
+            alert("Network Error")
         }
         orderInput.current.value = "";
     }
 
     function updateOrder(or) {
         console.log(or)
-        return(<>
-            <td>{or.orderId}</td>
-            <td>{or.customerId}</td>
-            <td>{or.orderDate}</td>
-            <td>{or.status}</td>
-            <td>{or.total}</td>
-            <td>{or.partId}</td>
-            <td>{or.quantity}</td>
-        </>)
+        
     }
 
     async function deleteOrder(o) {
@@ -252,72 +117,37 @@ export default function OrderTable() {
     }
     
 
-    const tableStyle = {
-        backgroundColor: "lightgrey",
-        border: "1px solid black"
-    }
-    useEffect(() => async function getAuthorizedUser(){
-        try {
-            const response = await fetch('http://localhost:8080/auth/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-               // body: JSON.stringify({'username', 'password'}),
-                
-            });
-            setAuthUser(response);
-
-        } catch(e) {
-            console.log(e);
-            console.log(authUser);
-        }
-    });
     return (<>
         
-        <button onClick={getAllOrders}>Get All Orders</button>     
-        <input type="number" ref={orderInput} />
-        <button onClick={viewOrder}>Enter Number of Order</button>
-
+        <div class="container">
+        <h1>View Orders</h1>
+            <div class="button-holder">
+            <input type="number" ref={orderInput} placeholder='Enter Order ID' id = "order-input"/>
+            <button onClick={viewOrder} class = "buttons">Click Here To Get Order</button>
+            <button onClick={getAllOrders} class = "buttons">Get All Orders</button> 
+            </div>  
+        </div>
+        
         {orders.length > 0 && (
-            <table style = {tableStyle}>
+            <Table striped bordered hover responsive="sm">
                 <thead>
                     <tr>
-                        <th>ID</th>
-                        <th>Customer ID</th>
-                        <th>Order Date</th>
-                        <th>Status</th>
-                        <th>Total</th>
-                        <th>Part ID</th>
-                        <th>Quantity</th>
-                        <th>Update Order</th>
-                        <th>Cancel Order</th>
+                        <td>ID</td>
+                        <td>Customer ID</td>
+                        <td>Order Date</td>
+                        <td>Status</td>
+                        <td>Total</td>
+                        <td>Part ID</td>
+                        <td>Quantity</td>
+                        <td>Update Order</td>
+                        <td>Cancel Order</td>
                     </tr>
                 </thead>
                 <tbody>
                     {orderTableRows}
                 </tbody>
-            </table>
+            </Table>
         )}
         </>)
 
 }
-
-
- // async function getAuthorizedUser(){
-    //     try {
-    //         const response = await fetch('http://localhost:8080/auth/login', {
-    //             method: 'POST',
-    //             headers: {
-    //                 'Content-Type': 'application/json',
-    //             },
-    //            // body: JSON.stringify({'username', 'password'}),
-                
-    //         });
-    //         setAuthUser(response);
-
-    //     } catch(e) {
-    //         console.log(e);
-    //         console.log(authUser);
-    //     }
-    // }
